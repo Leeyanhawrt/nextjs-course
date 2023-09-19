25f7eada35e2c94f171bee0a51ddf433f2d521eb
Leeyanhawrt <leeyan1108@gmail.com>
2023-09-19 14:53:07 -0400

    Add 404 page

--- pages/clients/[id]/index.js
+++ pages/clients/[id]/index.js
@@ -11,10 +11,12 @@ function ClientProjectsPage() {
     })
   }
 
-  return <div>
-    <h1>The Projects of a Given Client</h1>
-    <button onClick={loadProjectHandler}>Load Project A</button>
-  </div>
+  return (
+    <div>
+      <h1>The Projects of a Given Client</h1>
+      <button onClick={loadProjectHandler}>Load Project A</button>
+    </div>
+  )
 }
 
 export default ClientProjectsPage;
