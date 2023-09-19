e5889ebbbc23f7ba3decf3ea6eec65fdd214efca
Leeyanhawrt <leeyan1108@gmail.com>
2023-09-19 14:38:54 -0400

    Add dynamic route links

--- pages/clients/index.js
+++ pages/clients/index.js
@@ -1,7 +1,30 @@
+import Link from 'next/link'
+
 function ClientsPage() {
-  return <div>
-    <h1>The Clients Page</h1>
-  </div>
+  const clients = [
+    { id: 'max', name: 'Maximilian' },
+    { id: 'manu', name: 'Manuel' },
+  ]
+
+  const renderedClients = clients.map((client) => {
+    return (
+      <li key={client.id}>
+        <Link href={`/clients/${client.id}`}>{client.name}</Link>
+      </li>
+    )
+  })
+
+  console.log(renderedClients)
+
+
+  return (
+    <div>
+      <h1>The Clients Page</h1>
+      <ul>
+        {renderedClients}
+      </ul>
+    </div>
+  )
 }
 
 export default ClientsPage;
